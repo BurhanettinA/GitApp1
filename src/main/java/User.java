c8f71997ff931282b95da4baf43f11002b47dce9
burhanettin <burhanettin>
2024-09-19 21:07:11 +0200

    v4

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,5 +2,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("Versiyon3");
         System.out.println("Versiyon 3 devami");
+        System.out.println("Versiyon 4");
     }
 }
